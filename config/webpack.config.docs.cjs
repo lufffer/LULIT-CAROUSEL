6c6f850f8b8cce57de3480eaabd1f09419ba14f9
Luffer <lautaro.cajal@gmail.com>
2022-11-27 13:28:04 -0300

    adding docs/

--- config/webpack.config.docs.cjs
+++ config/webpack.config.docs.cjs
@@ -1,26 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 const TerserJSPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 const PATHS = {
   src: path.join(__dirname, "../src"),
   dist: path.join(__dirname, "../dist"),
+  public: path.join(__dirname, "../public"),
 };
 
 module.exports = {
   entry: {
-    "docs/lulit-carousel": "./src/lulit-carousel.ts",
+    "docs/lulit-carousel": path.join(__dirname, "../src", "lulit-carousel.js"),
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "../docs"),
-    clean: true,
+    path: path.resolve(__dirname, "../"),
   },
-  mode: "production",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
         exclude: /node_modules/,
       },
       {
@@ -30,14 +39,29 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(PATHS.public, "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.join(PATHS.src, "imgs"), to: "./imgs" },
+        {
+          from: path.join(PATHS.public, "assets", "css", "styles.css"),
+          to: path.join("public", "assets", "css", "styles.css"),
+        },
+      ],
+    }),
+  ],
   optimization: {
-    minimizer: [new TerserJSPlugin({ extractComments: false })],
+    minimizer: [new CssMinimizerWebpackPlugin(), new TerserJSPlugin({})],
   },
   devtool: "source-map",
   devServer: {
     static: PATHS.dist,
   },
   resolve: {
-    extensions: [".js", ".ts"],
+    extensions: [".css", ".js", ".ts"],
   },
 };
